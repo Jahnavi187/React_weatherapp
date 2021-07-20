602ce5e5f1533592566f965844823a354c1b64b8
Jahnavi <kathulajahnavi100.com>
2021-07-21 00:12:34 +0530

    new weather

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'weather-icons/css/weather-icons.css'; 
+import 'weather-icons/css/weather-icons.css';
 import Weather from './app_component/weather.component';
 import Form from './app_component/form.component.jsx';
-const API_key="8872851781328cd9bd14e4436109b781"; 
+const API_key = "8872851781328cd9bd14e4436109b781";
 
 
-class App extends React.Component{
-  constructor(){
+class App extends React.Component {
+  constructor() {
     super();
-    this.state={
+    this.state = {
       city: undefined,
-      country:undefined,
-      icon:undefined,
-      main:undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
       celsius: undefined,
       temp_max: undefined,
-      temp_min:undefined,
-      description:"",
+      temp_min: undefined,
+      description: "",
       wind_speed: "",
-      error:false
+      error: false
     };
-    
-    
-    this.weatherIcon={
-      Thunderstorm:"wi-thunderstorm",
+
+
+    this.weatherIcon = {
+      Thunderstorm: "wi-thunderstorm",
       Drizzle: "wi-sleet",
       Rain: "wi-storm-showers",
       Snow: "wi-snow",
       Atmosphere: "wi-fog",
       Clear: "wi-day-sunny",
       Clouds: "wi-day-fog"
-  };
+    };
   }
   get_WeatherIcon(icons, rangeId) {
     switch (true) {
@@ -61,58 +61,57 @@ class App extends React.Component{
         this.setState({ icon: this.weatherIcon.Clouds });
     }
   }
-  calcelsius(temp){
-    let cell=Math.floor(temp-273.15);
+  calcelsius(temp) {
+    let cell = Math.floor(temp - 273.15);
     return cell;
   }
-  getWeather=async(e)=>{
+  getWeather = async (e) => {
     e.preventDefault();
     const country = e.target.elements.country.value;
     const city = e.target.elements.city.value;
-    if(city &&country)
-{
-  const api_call= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}uk&appid=${API_key}`);
+    if (city && country) {
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}uk&appid=${API_key}`);
 
-    const response=await api_call.json();
-    
-    console.log(response);
-    this.setState({
-      city:`${response.name},${response.sys.country}`,
-     
-      celsius: this.calcelsius(response.main.temp),
-      temp_max:this.calcelsius(response.main.temp_max),
-      temp_min: this.calcelsius(response.main.temp_min),
-      description:response.weather[0].description,
-      wind_speed: response.wind.speed
-      
-      
-    });
-    this.get_WeatherIcon(this.weatherIcon,response.weather[0].id);
-  }else{
-    this.setState({error:true});
+      const response = await api_call.json();
+
+      console.log(response);
+      this.setState({
+        city: `${response.name},${response.sys.country}`,
+
+        celsius: this.calcelsius(response.main.temp),
+        temp_max: this.calcelsius(response.main.temp_max),
+        temp_min: this.calcelsius(response.main.temp_min),
+        description: response.weather[0].description,
+        wind_speed: response.wind.speed
+
+
+      });
+      this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+    } else {
+      this.setState({ error: true });
+    }
+
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Form loadweather={this.getWeather} />
+        <Weather city={this.state.city}
+          weatherIcon={this.state.icon}
+
+
+          temp_celsius={this.state.celsius}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
+          description={this.state.description}
+          wind_speed={this.state.wind_speed}
+
+        />
+      </div>
+
+    );
   }
-  
-};
-    
-  render(){
-  return(
-    <div className="App">
-    <Form loadweather={this.getWeather}/>
-      <Weather city={this.state.city} 
-      weatherIcon={this.state.icon}
-
-      
-      temp_celsius={this.state.celsius}
-      temp_max={this.state.temp_max}
-      temp_min={this.state.temp_min}
-      description={this.state.description}
-      wind_speed={this.state.wind_speed}
-      
-      />
-    </div>
-
-  );
-}
 }
 
 export default App;
--- src/app_component/form.component.jsx
+++ src/app_component/form.component.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import "./form.style.css";
-const Form= props=>{
-    return(
+const Form = props => {
+    return (
         <div className="container">
-          <h1>Weather App</h1>
-        <form onSubmit={props.loadweather} >
-        <div className="row">
-        
-        <div className="col-md-3 offset-md-2">
-            <input type="text"  className="from-control" name="city" autoComplete="off" placeholder="City"></input>
-        </div>
-        <div className="col-md-3">
-        <input type="text"  className="from-control" name="country" autoComplete="off" placeholder="Country"></input>
+            <h1>Weather App</h1>
+            <form onSubmit={props.loadweather} >
+                <div className="row">
 
+                    <div className="col-md-3 offset-md-2">
+                        <input type="text" className="from-control" name="city" autoComplete="off" placeholder="City"></input>
+                    </div>
+                    <div className="col-md-3">
+                        <input type="text" className="from-control" name="country" autoComplete="off" placeholder="Country"></input>
+
+                    </div>
+                    <div className="col-md-3 mt-md-0 text-md-left">
+                        <button className="btn btn-dark">Get weather</button>
+                    </div>
+                </div>
+            </form>
         </div>
-        <div className="col-md-3 mt-md-0 text-md-left">
-     <button className="btn btn-dark">Get weather</button>
-     </div>
-        </div>
-        </form>
-         </div>
     );
 };
 export default Form
